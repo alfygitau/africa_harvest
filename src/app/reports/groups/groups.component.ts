7745c8db230c40865746ae8fe732a47c5473fee4
alfygitau <alfygitau@gmail.com>
2024-07-17 11:07:46 +0300

    changes

--- src/app/reports/groups/groups.component.ts
+++ src/app/reports/groups/groups.component.ts
@@ -19,6 +19,10 @@ import { NgbPagination } from '@ng-bootstrap/ng-bootstrap';
 import { switchMap } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
+// PrimeNG Modules
+import { MultiSelectModule } from 'primeng/multiselect';
+import { CardModule } from 'primeng/card';
+
 @Component({
   selector: 'app-groups',
   standalone: true,
@@ -30,6 +34,8 @@ import { CommonModule } from '@angular/common';
     NgxDatatableModule,
     NgbPagination,
     CommonModule,
+    MultiSelectModule,
+    CardModule,
   ],
   templateUrl: './groups.component.html',
   styleUrl: './groups.component.scss',
@@ -51,6 +57,15 @@ export class GroupsComponent implements OnInit {
   public limit: number = 10;
   public totalGroups: number = 0;
 
+  public selectedCounty: any[] = [];
+  myCounties: any[] = [];
+  subcountyOptions = [{ subCountyId: 1, name: 'Select a subcounty' }];
+  wardOptions = [{ wardId: 1, name: 'Select a ward' }];
+  myGroups: any = [{ subCountyId: 1, name: 'Select a group' }];
+  selectedSubcounty: [] = [];
+  selectedWard: [] = [];
+  selectedGroup: [] = [];
+
   constructor(
     private formBuilder: FormBuilder,
     private cdr: ChangeDetectorRef,
@@ -62,10 +77,11 @@ export class GroupsComponent implements OnInit {
     const date = new Date();
     const startDate = new Date();
     startDate.setMonth(startDate.getMonth() - 1);
-    this.dataParams.page_num = 0;
+    this.dataParams.page_num = 1;
     this.dataParams.page_size = 10;
 
     this.counties = counties;
+    this.myCounties = this.transformCounties(counties);
     this.breadCrumbItems = [
       { label: 'Reports' },
       { label: 'Groups', active: true },
@@ -80,12 +96,84 @@ export class GroupsComponent implements OnInit {
       groupId: [[], Validators.required],
     });
 
-    // Trigger fetch when form changes or on initialization
-    this.searchForm.valueChanges
-      .pipe(switchMap(async () => this.getGroups()))
-      .subscribe();
-
     this.getGroups();
+
+    this.searchForm.valueChanges.subscribe(() => {
+      let obj = {
+        countyId: this.searchForm
+          .get('countyId')
+          ?.value.map((county: any) => county.county_id),
+        subCountyId: this.searchForm
+          .get('subCountyId')
+          ?.value.map((subCounty: any) => subCounty.subCountyId),
+        wardId: this.searchForm
+          .get('wardId')
+          ?.value.map((ward: any) => ward.wardId),
+        groupId: this.searchForm
+          .get('groupId')
+          ?.value.map((group: any) => group.group_id),
+        startDate: this.searchForm.get('start_date')?.value
+          ? this.searchForm.get('start_date')?.value
+          : '',
+        endDate: this.searchForm.get('end_date')?.value
+          ? this.searchForm.get('end_date')?.value
+          : '',
+      };
+
+      let data = {
+        page: this.dataParams.page_num,
+        dataObj: {
+          countyId: this.searchForm
+            .get('countyId')
+            ?.value.map((county: any) => county.county_id),
+          subCountyId: this.searchForm
+            .get('subCountyId')
+            ?.value.map((subCounty: any) => subCounty.subCountyId),
+          wardId: this.searchForm
+            .get('wardId')
+            ?.value.map((ward: any) => ward.wardId),
+          startDate: this.searchForm.get('start_date')?.value
+            ? this.searchForm.get('start_date')?.value
+            : '',
+          endDate: this.searchForm.get('end_date')?.value
+            ? this.searchForm.get('end_date')?.value
+            : '',
+        },
+        size: this.dataParams.page_size,
+      };
+      this.filterGroups(obj);
+    });
+  }
+
+  filter(value: any) {
+    const selectedCountyIds = value?.map((county: any) => county.county_id);
+    if (selectedCountyIds) {
+      const filteredSubcounties = this.counties
+        .filter((county) => selectedCountyIds.includes(county.county_id))
+        .flatMap((county) => county.sub_counties);
+      console.log(filteredSubcounties);
+      this.subcountyOptions = [
+        { subCountyId: 1, name: 'Select a subcounty' },
+        ...filteredSubcounties,
+      ];
+      this.wardOptions = [{ wardId: 1, name: 'Select a ward' }];
+    }
+  }
+
+  filterWards(selectedSubcounties: any[]) {
+    this.wardOptions = [{ wardId: 1, name: 'Select a ward' }];
+    selectedSubcounties.forEach((subcounty) => {
+      if (subcounty && subcounty.wards) {
+        this.wardOptions.push(...subcounty.wards);
+      }
+    });
+  }
+
+  transformCounties(data: any) {
+    return data.map((county: any) => ({
+      label: county.name,
+      value: county.county_id,
+    }));
   }
 
   edit(row: any) {
@@ -116,44 +204,28 @@ export class GroupsComponent implements OnInit {
   }
 
   getGroups() {
-    // this.spinner.show()
     this.groupsService.getGroups(this.size, this.limit).subscribe((res) => {
       if (res.statusCode == 200) {
         this.totalGroups = 200;
         this.groups = res.message;
         this.cdr.markForCheck();
-        // this.spinner.hide()
       }
     });
   }
 
-  onSubmit() {
-    // this.service.searchSalesReport(this.searchForm.value).subscribe((res)=>{
-    //   this.rows = res;
-    //     this.ref.markForCheck();
-    // })
-  }
+  onSubmit() {}
 
-  filtergroups(event: any) {
-    let object = {
-      countyId: this.searchForm.get('countyId')?.value,
-      subCountyId: this.searchForm.get('subCountyId')?.value,
-      wardId: this.searchForm.get('wardId')?.value,
-      startDate: '',
-      endDate: '',
-    };
-    this.groupsService.getGroupsByLocation(object).subscribe((res) => {
+  filtergroups(data: any) {
+    this.groupsService.getGroupsByLocation(data).subscribe((res) => {
       if (res.statusCode == 200) {
         console.log(res);
         this.groups = res.message;
         this.cdr.markForCheck();
-        // this.spinner.hide
       }
     });
   }
 
   filterGroups(data: any) {
-    console.log(data);
     this.groupsService.getGroupsByLocation(data).subscribe((res) => {
       if (res.statusCode == 200) {
         this.groups = res.message;
@@ -184,17 +256,17 @@ export class GroupsComponent implements OnInit {
     this.search();
   }
 
-  filterWards(event: Event) {
-    let ids = this.searchForm.get('subCountyId')?.value;
-    let filtered_array = this.sub_counties.filter((obj: any) =>
-      ids.includes(obj.subCountyId)
-    );
-    filtered_array.forEach((element) => {
-      this.wards = this.wards.concat(element.wards);
-    });
+  // filterWards(event: Event) {
+  //   let ids = this.searchForm.get('subCountyId')?.value;
+  //   let filtered_array = this.sub_counties.filter((obj: any) =>
+  //     ids.includes(obj.subCountyId)
+  //   );
+  //   filtered_array.forEach((element) => {
+  //     this.wards = this.wards.concat(element.wards);
+  //   });
 
-    this.search();
-  }
+  //   this.search();
+  // }
 
   getWards() {
     let ids = this.searchForm.get('subCountyId')?.value;
